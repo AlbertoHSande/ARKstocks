62551645b5ca9936e237582dfeb753ea42ffc40b
Alberto Hernández Sande <albertohsande@gmail.com>
2021-11-18 22:46:01 +0100

    Create install.sh
    Notes for easy install in the future

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt install python-pip3
+pip3 install opencv-contrib-python; sudo apt-get install -y libatlas-base-dev libhdf5-dev libhdf5-serial-dev libatlas-base-dev libjasper-dev libqtgui4 libqt4-test
+pip3 install pandas
+pip3 install cloudscraper
+pip3 install twython
+pip3 install bokeh
